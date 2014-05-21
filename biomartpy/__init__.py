8b435fb26dfc9ed39944529f3b287a566adc7b2f
daler <dalerr@niddk.nih.gov>
2014-05-21 16:15:16 -0400

    update tests for pandas 0.13.1 output

--- biomartpy/__init__.py
+++ biomartpy/__init__.py
@@ -33,14 +33,15 @@ def list_marts():
     List available marts
 
     >>> list_marts().ix[0:5]
-                   biomart                            version
-    0              ensembl       ENSEMBL GENES 69 (SANGER UK)
-    1                  snp   ENSEMBL VARIATION 69 (SANGER UK)
-    2  functional_genomics  ENSEMBL REGULATION 69 (SANGER UK)
-    3                 vega               VEGA 49  (SANGER UK)
-    4     bacteria_mart_16       ENSEMBL BACTERIA 16 (EBI UK)
-    5        fungi_mart_16          ENSEMBL FUNGI 16 (EBI UK)
-
+                   biomart                              version
+    0              ensembl         ENSEMBL GENES 75 (SANGER UK)
+    1                  snp     ENSEMBL VARIATION 75 (SANGER UK)
+    2  functional_genomics    ENSEMBL REGULATION 75 (SANGER UK)
+    3                 vega                 VEGA 53  (SANGER UK)
+    4        fungi_mart_21            ENSEMBL FUNGI 21 (EBI UK)
+    5  fungi_variations_21  ENSEMBL FUNGI VARIATION 21 (EBI UK)
+    <BLANKLINE>
+    [6 rows x 2 columns]
     """
 
     return rpy2_to_pandas(r.listMarts())
@@ -51,9 +52,15 @@ def list_datasets(mart_name, verbose=False):
     Returns a pandas.DataFrame listing datasets in mart name
 
     >>> list_datasets('ensembl').ix[6:7]
-                       dataset                       description    version
-    6  mlucifugus_gene_ensembl  Myotis lucifugus genes (myoLuc2)    myoLuc2
-    7    hsapiens_gene_ensembl    Homo sapiens genes (GRCh37.p8)  GRCh37.p8
+                      dataset                          description  \\
+    6  csavignyi_gene_ensembl       Ciona savignyi genes (CSAV2.0)   
+    7     fcatus_gene_ensembl  Felis catus genes (Felis_catus_6.2)   
+    <BLANKLINE>
+               version  
+    6          CSAV2.0  
+    7  Felis_catus_6.2  
+    <BLANKLINE>
+    [2 rows x 3 columns]
     """
     return rpy2_to_pandas(r.listDatasets(mart=r.useMart(mart_name), verbose=verbose))
 
@@ -69,7 +76,8 @@ def list_attributes(mart_name, dataset):
     0        ensembl_gene_id        Ensembl Gene ID
     1  ensembl_transcript_id  Ensembl Transcript ID
     2     ensembl_peptide_id     Ensembl Protein ID
-
+    <BLANKLINE>
+    [3 rows x 2 columns]
     """
     dataset = r.useDataset(dataset, mart=r.useMart(mart_name))
     return rpy2_to_pandas(r.listAttributes(dataset))
@@ -86,6 +94,8 @@ def list_filters(mart_name, dataset):
     0  chromosome_name  Chromosome name
     1            start  Gene Start (bp)
     2              end    Gene End (bp)
+    <BLANKLINE>
+    [3 rows x 2 columns]
 
     """
     dataset = r.useDataset(dataset, mart=r.useMart(mart_name))
@@ -144,13 +154,17 @@ def make_lookup(mart_name, dataset, attributes, filters=None, values=None,
     FBgn0031208              CG11023              2L
     FBgn0031209                Ir21a              2L
     FBgn0051973                 Cda5              2L
+    <BLANKLINE>
+    [4 rows x 2 columns]
+
 
     Indexing by gene ID (or whatever was the first attribute provided):
 
     >>> df.ix['FBgn0031209']
     flybasename_gene    Ir21a
     chromosome_name        2L
-    Name: FBgn0031209
+    Name: FBgn0031209, dtype: object
+
 
 
     Extracting data:
@@ -166,7 +180,7 @@ def make_lookup(mart_name, dataset, attributes, filters=None, values=None,
     FBgn0031208        CG11023
     FBgn0031209          Ir21a
     FBgn0051973           Cda5
-    Name: flybasename_gene
+    Name: flybasename_gene, dtype: object
 
     """
     def _filter_and_values_to_RList(d):
